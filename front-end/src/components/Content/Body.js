5b168e5489aec64ffa03f2e95dc6b3d022d4e6a0
net2katsu <net2katsu1507@yahoo.co.jp>
2022-03-03 08:16:02 +0900

    カラムのコンポーネント作成

---
+++ front-end/src/components/Content/Body.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./style.css";
+export default function Body() {
+  const columns = 12;
+  const item = (
+    <div class="item">
+      <img src={'./images/card-image.png'} class="box-image" />
+      <h3 className="title">Card Title</h3>
+      <p className="context">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+    </div>
+  );
+
+  let items = [];
+  for (let i = 0; i < columns; i++) {
+    items.push(item);
+  }
+
+  return (
+    <div class="items">
+      {items}
+    </div>
+  );
+}
---
+++ front-end/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+export default function Header() {
+  return (
+    <header>
+      <img src='./images/test.jpg' className='test-image'></img>
+      Header components
+    </header>
+  );
+}
+
