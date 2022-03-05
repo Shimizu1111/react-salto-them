1bcfb2fa5d22051cbcaf8a04c9966318141aab7a
Shimizu1111 <foralbaito@yahoo.co.jp>
2022-03-05 15:21:35 +0900

    ヘッダーコンポーネント作成

--- front-end/src/components/Header/Header.js
+++ front-end/src/components/Header/Header.js
@@ -5,14 +5,20 @@ export default function Header() {
   return (
     <header>
       <img src='./images/Logo.png' className='test-image' />
-      <div class="search_box">
-        <input type="text" placeholder="search" />
+      <div class='search_box'>
+        <input type='text' placeholder='search' />
       </div>
       Header components
       <div className='container_right_justified'>
         <img src='./images/Ellipse176.png' className='ellipse' />
+        <img src='./images/Notification.png' className='notification' />
+        <img src='./images/Group.png' className='group' />
+        <img src='./images/Beared-guy.png' className='beared-guy' />
+        <div className='profile'>
+          <p>shimiu katsunori</p>
+          <p>admin</p>
+        </div>
       </div>
     </header>
   );
 }
-
