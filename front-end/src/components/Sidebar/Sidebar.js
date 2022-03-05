c62ec1f05895111a8860a0348456749b478a059a
Shimizu1111 <foralbaito@yahoo.co.jp>
2022-03-05 23:15:49 +0900

    サイドバーのコメント追加と使わないロジック除去

--- front-end/src/components/Sidebar/Sidebar.js
+++ front-end/src/components/Sidebar/Sidebar.js
@@ -3,8 +3,8 @@ import './style.scss';
 
 export default function Sidebar() {
   const sidebar_components_name  = [
-    {id: 1, title: 'Main', class: 'sidebar_main', row: ['Users', 'Posts', 'Categories'], image: ['Dashbord.png', 'Dashbord.png', 'Dashbord.png']},
-    {id: 2, title: 'Settings', class: 'sidebar_settings', row: ['Profile', 'Roles', 'Maps'], image: ['Dashbord.png', 'Dashbord.png', 'Dashbord.png']}
+    {title: 'Main', class: 'sidebar_main', row: ['Users', 'Posts', 'Categories'], image: ['Dashbord.png', 'Dashbord.png', 'Dashbord.png']},
+    {title: 'Settings', class: 'sidebar_settings', row: ['Profile', 'Roles', 'Maps'], image: ['Dashbord.png', 'Dashbord.png', 'Dashbord.png']}
   ];
 
   /** Main・Settingsなどのコンテンツのリンク先を表示する項目
@@ -13,36 +13,31 @@ export default function Sidebar() {
    */
   const sidebar_content_inner = (content_row, content_image) => {
     return(
-      <>
-        <div className='sidebar_main_inner'>
-          <img src={'./images/' + content_image}  className='users-image' />
-          <p>{content_row}</p>
-        </div>
-      </>
+      <div className='sidebar_main_inner'>
+        <img src={'./images/' + content_image}  className='users-image' />
+        <p>{content_row}</p>
+      </div>
     )
   };
 
-
   // MainやSettingsなどコンテンツごとに表示する項目
   const sidebar_components = sidebar_components_name.map((sidebar_component_name) =>
-    <React.Fragment key={sidebar_component_name.id}>
-      <div className={sidebar_component_name.class}>
-        {sidebar_component_name.title}
-        {sidebar_component_name.row.map((component_row_name, i) => 
-           sidebar_content_inner(component_row_name, sidebar_component_name.image[i])
-        )}
-      </div>
-    </React.Fragment>
+    <div className={sidebar_component_name.class}>
+      {sidebar_component_name.title}
+      {sidebar_component_name.row.map((component_row_name, i) => 
+          sidebar_content_inner(component_row_name, sidebar_component_name.image[i])
+      )}
+    </div>
   );
 
   /**サイドバーの表示項目
    * ・Dashbord
    * 　・Dashbord
-   * ・Main
+   * ・Main        => {sidebar_components} で画面表示
    * 　・Users
    * 　・Posts
    * 　・Categories
-   * ・Settings
+   * ・Settings    => {sidebar_components} で画面表示
    * 　・Profile
    * 　・Roles
    * 　・Maps
