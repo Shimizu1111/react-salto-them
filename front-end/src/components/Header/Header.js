2035f4ddaaa1db00ed78624e507ae892c69a1bcd
net2katsu <net2katsu1507@yahoo.co.jp>
2022-03-05 12:02:04 +0900

    ヘッダーの改修のため検索バーを追加

--- front-end/src/components/Header/Header.js
+++ front-end/src/components/Header/Header.js
@@ -4,8 +4,14 @@ import './style.scss';
 export default function Header() {
   return (
     <header>
-      <img src='./images/test.jpg' className='test-image'></img>
+      <img src='./images/Logo.png' className='test-image' />
+      <div class="search_box">
+        <input type="text" placeholder="search" />
+      </div>
       Header components
+      <div className='container_right_justified'>
+        <img src='./images/Ellipse176.png' className='ellipse' />
+      </div>
     </header>
   );
 }
