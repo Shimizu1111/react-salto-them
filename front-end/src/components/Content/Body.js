07ea86cbeb43e7716fc1e29602e3c41a9506dddb
Shimizu1111 <foralbaito@yahoo.co.jp>
2022-03-07 21:33:16 +0900

    Bodyの一部修正

--- front-end/src/components/Content/Body.js
+++ front-end/src/components/Content/Body.js
@@ -4,7 +4,7 @@ export default function Body() {
   const columns = 12;
   const item = (
     <div class="item">
-      <img src={'./images/card-image.png'} class="box-image" />
+      <img src={'./images/card-image.png'} className="box-image" />
       <h3 className="title">Card Title</h3>
       <p className="context">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
     </div>
