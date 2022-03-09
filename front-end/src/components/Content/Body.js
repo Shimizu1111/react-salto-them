1f2ac9a14dcae43320c6c16b7b51176716360cfa
Shimizu1111 <foralbaito@yahoo.co.jp>
2022-03-10 07:42:23 +0900

    Userページの大枠完成

--- front-end/src/components/Content/Body.js
+++ front-end/src/components/Content/Body.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.scss";
-import BreadCrumb from "./bread-crumb/bread-crumb";
+import BreadCrumb from "./Bread-crumb/bread-crumb";
+import Pagination from "./Pagination/Pagination";
 
 export default function Body() {
   const columns = 12;
@@ -23,23 +24,7 @@ export default function Body() {
       <div className="items">
         {items}
       </div>
-      <div className="pagination-wrapper">
-        <p>Total 85 items</p>
-        <ul class="pagination">
-          <li><a href="#">«</a></li>
-          <li><a href="#">1</a></li>
-          <li><a>...</a></li>
-          <li><a href="#">4</a></li>
-          <li><a href="#">5</a></li>
-          <li><a class="active" href="#">6</a></li>
-          <li><a href="#">7</a></li>
-          <li><a href="#">8</a></li>
-          <li><a>...</a></li>
-          <li><a href="#">20</a></li>
-          <li><a href="#">»</a></li>
-        </ul>
-      </div>
+      <Pagination />
     </div>
-    
   );
 }
--- front-end/src/components/Content/User/User.js
+++ front-end/src/components/Content/User/User.js
@@ -4,7 +4,8 @@ import Header from "../../Header/Header";
 import Sidebar from "../../Sidebar/Sidebar";
 import Body from "../Body";
 import Footer from "../../Footer/Footer";
-import BreadCrumb from "../bread-crumb/bread-crumb";
+import BreadCrumb from "../Bread-crumb/bread-crumb";
+import Pagination from "../Pagination/Pagination";
 
 export default function User () {
   const data = [
@@ -13,79 +14,118 @@ export default function User () {
       title: "::",
       username: "Textline",
       emali: "Textline",
-      status: "true",
+      status: "Active",
     },
     {
       checkbox: false,
       title: "::",
       username: "Textline",
       emali: "Textline",
-      status: "true",
+      status: "Unactive",
     },
     {
       checkbox: false,
       title: "::",
       username: "Textline",
       emali: "Textline",
-      status: "true",
+      status: "Active",
     },
     {
       checkbox: false,
       title: "::",
       username: "Textline",
       emali: "Textline",
-      status: "true",
+      status: "Textline",
     },
     {
       checkbox: false,
       title: "::",
       username: "Textline",
       emali: "Textline",
-      status: "true",
+      status: "Textline",
+    },
+    {
+      checkbox: false,
+      title: "::",
+      username: "Textline",
+      emali: "Textline",
+      status: "Textline",
+    },
+    {
+      checkbox: false,
+      title: "::",
+      username: "Textline",
+      emali: "Textline",
+      status: "Textline",
+    },
+    {
+      checkbox: false,
+      title: "::",
+      username: "Textline",
+      emali: "Textline",
+      status: "Textline",
+    },
+    {
+      checkbox: false,
+      title: "::",
+      username: "Textline",
+      emali: "Textline",
+      status: "Textline",
+    },
+    {
+      checkbox: false,
+      title: "::",
+      username: "Textline",
+      emali: "Textline",
+      status: "Textline",
     },
   ];
 
   return (
     <>
       <Header />
-
       <div className='container'>
         <Sidebar />
-        {/* <Body /> */}
-        <table>
-          <tbody>
-            <tr>
-              <th class="checkbox-Area">
-                <input type="checkbox" name="scales" />
-              </th>
-              <th class="title_Area">Title</th>
-              <th class="username_Area">UserName</th>
-              <th class="email_Area">Email</th>
-              <th class="status_Area">Status</th>
-              <th>Title</th>
-            </tr>
-            {data.map((item) => {
-              return (
+        <div className="right-wrapper">
+          <BreadCrumb />
+          <div className="wrapper">
+            <table>
+              <tbody>
                 <tr>
-                  <td class="first_Area">
-                    <input type="checkbox" checked={item.checkbox} name="scales" />
-                  </td>
-                  <td>{item.title}</td>
-                  <td>{item.username}</td>
-                  <td>{item.emali}</td>
-                  <td>{item.status}</td>
-                  <td>
-                    <a href="#">Publish</a>
-                    <a href="#">Edit</a>
-                    <a href="#">Delete</a>
-                  </td>
+                  <th class="checkbox-Area">
+                    <input type="checkbox" name="scales" />
+                  </th>
+                  <th class="title_Area">Title</th>
+                  <th class="username_Area">UserName</th>
+                  <th class="email_Area">Email</th>
+                  <th class="status_Area">Status</th>
+                  <th>Actions</th>
                 </tr>
-              );
-            })}
-          </tbody>
-        </table>
+                {data.map((item) => {
+                  return (
+                    <tr>
+                      <td class="first_Area">
+                        <input type="checkbox" checked={item.checkbox} name="scales" />
+                      </td>
+                      <td>{item.title}</td>
+                      <td>{item.username}</td>
+                      <td>{item.emali}</td>
+                      <td>{item.status}</td>
+                      <td>
+                        <a href="#">Publish</a>
+                        <a href="#">Edit</a>
+                        <a href="#">Delete</a>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+            <Pagination />
+            <Footer />
+          </div>
+        </div>
       </div>
-      <Footer />
     </>
   );
 }
