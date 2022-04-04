2900154e0238dcb52864876cf9e870e885c6d0ed
Shimizu1111 <foralbaito@yahoo.co.jp>
2022-04-04 23:42:57 +0900

    apiとページネーションの実装

--- front-end/src/components/Content/User/User.js
+++ front-end/src/components/Content/User/User.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.scss"
 import Header from "../../Header/Header";
 import Sidebar from "../../Sidebar/Sidebar";
@@ -6,80 +6,49 @@ import Body from "../Body";
 import Footer from "../../Footer/Footer";
 import BreadCrumb from "../bread-crumb/bread-crumb";
 import Pagination from "../Pagination/Pagination";
+import { fetchAsGet } from "../../../services/api";
+import { API } from "../../../configs/constant";
+import MuiPagination from '@material-ui/lab/Pagination';
+import { withStyles } from '@material-ui/core/styles';
+
 
 export default function User () {
-  const data = [
-    {
-      checkbox: false,
-      title: "::",
-      username: "Textline",
-      emali: "Textline",
-      status: "Active",
+  //ページ番号
+  const [page, setPage] = useState(1)
+
+  const Pagination = withStyles({
+    root: {
+      display: 'inline-block',  //中央寄せのためインラインブロックに変更
     },
-    {
-      checkbox: false,
-      title: "::",
-      username: "Textline",
-      emali: "Textline",
-      status: "Unactive",
-    },
-    {
-      checkbox: false,
-      title: "::",
-      username: "Textline",
-      emali: "Textline",
-      status: "Active",
-    },
-    {
-      checkbox: false,
-      title: "::",
-      username: "Textline",
-      emali: "Textline",
-      status: "Textline",
-    },
-    {
-      checkbox: false,
-      title: "::",
-      username: "Textline",
-      emali: "Textline",
-      status: "Textline",
-    },
-    {
-      checkbox: false,
-      title: "::",
-      username: "Textline",
-      emali: "Textline",
-      status: "Textline",
-    },
-    {
-      checkbox: false,
-      title: "::",
-      username: "Textline",
-      emali: "Textline",
-      status: "Textline",
-    },
-    {
-      checkbox: false,
-      title: "::",
-      username: "Textline",
-      emali: "Textline",
-      status: "Textline",
-    },
-    {
-      checkbox: false,
-      title: "::",
-      username: "Textline",
-      emali: "Textline",
-      status: "Textline",
-    },
-    {
-      checkbox: false,
-      title: "::",
-      username: "Textline",
-      emali: "Textline",
-      status: "Textline",
-    },
-  ];
+  })(MuiPagination);
+
+  const [users, setData] = useState([]);
+  // const [page, setPage] = useState(1);
+    const token = "";
+
+    console.log(process.env);
+    useEffect(() => {
+      async function fetchData() {
+        const url = `${API.USER.GET}?page=${page}`;
+        console.log("urlの直後");
+        console.log(url);
+        const res = await fetch(url, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        });
+        
+  
+        const users = await res.json();
+        setData(users.data);
+      }
+  
+      fetchData();
+    }, [page]);
+    
 
   return (
     <>
@@ -101,26 +70,39 @@ export default function User () {
                   <th class="status_Area">Status</th>
                   <th>Actions</th>
                 </tr>
-                {data.map((item) => {
+                {users.map((user) => {
                   return (
-                    <tr key={item}>
+                    <tr key={user}>
                       <td class="first_Area">
-                        <input type="checkbox" checked={item.checkbox} name="scales" />
+                        <input type="checkbox" checked={user.checkbox} name="scales" />
                       </td>
-                      <td>{item.title}</td>
-                      <td>{item.username}</td>
-                      <td>{item.emali}</td>
-                      <td>{item.status}</td>
+                      <td>{user.id}</td>
+                      <td>{user.name}</td>
+                      <td>{user.emali}</td>
+                      <td>{user.status}</td>
                       <td>
                         <a href="#">Publish</a>
                         <a href="#">Edit</a>
                         <a href="#">Delete</a>
                       </td>
                     </tr>
+                    
                   );
                 })}
+                
+                {console.log("helloo")}
+                {console.log(users)}
+                {console.log(page)}
               </tbody>
             </table>
+            <div style={{textAlign: "center"}}>
+              <Pagination 
+                count={10}          //総ページ数
+                color="primary"     //ページネーションの色
+                onChange={(e, page) =>setPage(page)}  //変更されたときに走る関数。第2引数にページ番号が入る
+                page={page}         //現在のページ番号
+                />
+            </div>
             <Pagination />
             </div>
           <Footer />
