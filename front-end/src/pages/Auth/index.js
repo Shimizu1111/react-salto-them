ae6bb863756379a2ba1fe0cf795b89a41ac3d096
Shimizu1111 <foralbaito@yahoo.co.jp>
2022-04-25 22:10:24 +0900

    バリデーションのバグ修正

--- front-end/src/pages/Auth/index.js
+++ front-end/src/pages/Auth/index.js
@@ -4,75 +4,59 @@ import LoadingButton from '@mui/lab/LoadingButton';
 import { 
   Avatar,
   Box, 
-  Button, 
   Container, 
   createTheme, 
   CssBaseline, 
-  Grid, 
   TextField, 
   ThemeProvider, 
   Typography 
 } from "@mui/material"
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { API } from "../../configs/constant";
-import { useSelector, useDispatch } from 'react-redux'
-import { login } from "../../services/api";
-import { RootState } from '../../redux/store/store'
-import { counterSlice, increment, decrement } from "../../redux/action/signin";
 
-/**
-  - [x] ログイン処理
-    - [x] ログイン画面からボタン押下
-      - [x] ユーザ情報確認（API叩く）
-      - [x] ユーザ情報あれば
-        - [x] ユーザ情報保持（トークン・ユーザ名・権限）
-        - [x] ダッシュボードへ画面遷移
-      - [x] ユーザ情報ない
-        - [x] ログインエラー時のリダイレクト処理
-      - [x] トークンをreduxのstoreに登録する
-      - [x] dispatchにトークンは仕込めたので、ユーザー名も入れられるようにする
-      - [ ] その他バリデーション
-        - [ ] 空白チェック
-    - [x] ログイン画面以外からURL直打ちした場合にログイン画面に遷移させる
-      - [x] トークンチェック
-        - [x] トークンがある場合にリンク先に遷移
-        - [x] トークンがない場合にログイン画面に遷移
-    - [x] ログアウト機能
-      - [x] トークンの破棄
-      - [ ] ポップアップ(ログアウトの確認、ログアウトしましたのポップアップ(数秒で自動で消える))
-    - [x] アイコン表示/非表示切り替え機能
-      - [x] ダッシュボードのリンク先を作成
-      - [x] ダッシュボード画面のページを作成
-      - [x] ボタンを押したらアイコンの切り替えを行う
-*/
-// ログイン処理
 export default function SignIn() {
   console.log("ログイン処理");
   const theme = createTheme();
   const navigate = useNavigate();
-  const dispatch = useDispatch();
 
+  // フォームの入力情報
+  const [values, setValues] = useState({
+    email: "",
+    password: ""
+  });
+
+  // メールのバリデーションエラー
+  const [ mail, setMail ] = useState({
+    error: false,
+    errorText: ""
+  })
+
+  // パスワードのバリデーションエラー
+  const [ password, setPassword ] = useState({
+    error: false,
+    errorText: ""
+  })
+
+  // ログイン処理
   async function handleLogin() {
     const url = `${API.USER.LOGIN}`
     // "katsunori.shimizu@salto.link123456"
     const data = { email: values.email, password: values.password };
- 
-    setError({...errors, emailText: "", email: false, passwordText: "", password: false});
 
-    // if (data.email == "" && data.password == "") {
-    //   setError({...errors, emailText: "メールの入力が行われていません", email: true, passwordText: "パスワードの入力が行われていません", password: true});
-    //   return
-    // }
+    if(values.email == "" && values.password == "") {
+      setMail({...mail, error: true, errorText: "メールの入力が行われていません"});
+      setPassword({...password, error: true, errorText: "パスワードの入力が行われていません"});
+    }
 
-    if(errors.email == "") {
-      setError({...errors, emailText: "メールの入力が行われていません", email: true});
-      console.log(errors)
+    if(values.email == "") {
+      setMail({...mail, error: true, errorText: "メールの入力が行われていません"});
+      setPassword({...password, error: false, errorText: ""});
       return
     }
 
-    if(errors.password == "") {
-      setError({...errors, passwordText: "パスワードの入力が行われていません", password: true});
-      console.log(errors);
+    if(values.password == "") {
+      setMail({...mail, error: false, errorText: ""});
+      setPassword({...password, error: true, errorText: "パスワードの入力が行われていません"});
       return
     }
     
@@ -97,18 +81,11 @@ export default function SignIn() {
       return navigate(("/users"), { replace: true });
     } catch(e) {
       console.log("エラーが吐かれました");
-      dispatch({type:"signin/signinName", payload: 5});
-      dispatch({type:"signin/signinId", payload: 3});
       console.log(e);
-      setError({...errors, emailText: "メールまたはパスワードが異なります", email: true, passwordText: "メールまたはパスワードが異なります", password: true});
+      setMail({...mail, error: true, errorText: "メールまたはパスワードが異なります"});
+      setPassword({...password, error: true, errorText: "メールまたはパスワードが異なります"});
     }
   }
-  
-  // フォームの入力情報
-  const [values, setValues] = useState({
-    email: "",
-    password: ""
-  });
 
   // フォームの入力を監視
   function handleInputChange(e) {
@@ -117,16 +94,6 @@ export default function SignIn() {
     const name = target.name;
     setValues({...values, [name]: value});
   }
-
-  // バリデーションエラーの情報
-  const [ errors, setError ] = useState({
-    email: false,
-    emailText: "",
-    password: false,
-    passwordText: ""
-  });
-
-  
 
   return(
     <ThemeProvider theme={theme}>
@@ -157,10 +124,9 @@ export default function SignIn() {
               autoComplete="email"
               onChange={(event) => handleInputChange(event)}
               value={values.email}
-              helperText={errors.emailText}
-              error={errors.email}
+              helperText={mail.errorText}
+              error={mail.error}
             />
-
             <TextField
               margin="normal"
               required
@@ -171,8 +137,8 @@ export default function SignIn() {
               type="password"
               onChange={(event) => handleInputChange(event)}
               value={values.password}
-              helperText={errors.passwordText}
-              error={errors.password}
+              helperText={password.errorText}
+              error={password.error}
             />
             <LoadingButton
               sx={{ mt: 2, mb: 2 }}
@@ -190,5 +156,4 @@ export default function SignIn() {
       </Container>
     </ThemeProvider>
   )
-
 }
